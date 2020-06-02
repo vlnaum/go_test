3b1db7c32a4e6b645a23b3e833f8cb9f39dd465f
vlnaum <vlnaum37@gmail.com>
2020-06-03 01:58:10 +0300

    init

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,45 @@ var (
 )
 
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	input, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer input.Close()
+
+	inputInfo, err := input.Stat()
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case inputInfo.Size() == 0 || !inputInfo.Mode().IsRegular():
+		return ErrUnsupportedFile
+	case inputInfo.Size() < offset:
+		return ErrOffsetExceedsFileSize
+	case limit == 0 || inputInfo.Size() < limit:
+		limit = inputInfo.Size()
+	}
+
+	output, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	if _, err := input.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(input)
+
+	if _, err := io.CopyN(output, barReader, limit); err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	tempFile, err := ioutil.TempFile("", "*")
+	if err != nil {
+		return
+	}
+	defer os.Remove(tempFile.Name())
+
+	t.Run("positive case", func(t *testing.T) {
+		result := Copy("testdata/test.txt", tempFile.Name(), 0, 0)
+		tempFileStat, _ := tempFile.Stat()
+
+		require.Nil(t, result)
+		require.Equal(t, int32(10), int32(tempFileStat.Size()))
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		result := Copy("testdata/test.txt", tempFile.Name(), 11, 0)
+
+		require.Equal(t, ErrOffsetExceedsFileSize, result)
+	})
+
+	t.Run("limit exceeds file size", func(t *testing.T) {
+		result := Copy("testdata/test.txt", tempFile.Name(), 0, 100)
+		tempFileStat, _ := tempFile.Stat()
+
+		require.Nil(t, result)
+		require.Equal(t, int32(10), int32(tempFileStat.Size()))
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		result := Copy("testdata", tempFile.Name(), 0, 0)
+		require.Equal(t, ErrUnsupportedFile, result)
+	})
 }
